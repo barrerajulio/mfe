b8fc69593ca3220486af4f2181ba89ae86dd12b5
Julio César Barrera Andrade <julio.barrera@yaydoo.com>
2020-12-30 06:18:39 -0600

    ci: automatically invalidation cludfront

--- container/src/App.js
+++ container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Bienvenido</h1>
+      <h1>Welcome!!</h1>
       <hr />
       <MarketingApp />
     </div>
